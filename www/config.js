d0fd0460f98f114c66c9154fc7c2753e2c4e8c42
Jen Bourey <jennifer.bourey@gmail.com>
2012-07-11 11:15:14 -0700

    Simplifying HTML5 configuration

--- www/config.js
+++ www/config.js
@@ -1,21 +1,19 @@
 var config = {};
         
-// base server configuration
+/* BASE SERVER URL */
 config.uMobileServerUrl = "http://10.0.2.2:8080";   // android only, otherwise http://localhost:8080
 //config.uMobileServerUrl = "https://umobile.unicon.net";   // android only, otherwise http://localhost:8080
+//config.uMobileServerUrl = "http://localhost/~me/umobile/www";  // sample HTML5-only testing URL
+
+/* UMOBILE SERVER CONTEXT PATH */
 config.uMobileServerContext = "";
-config.casServerUrl = "http://localhost:8080";
 
 // authentication
 config.loginFn = "localLogin";      // valid choices are localLogin or casLogin
 config.encryptionKey = "umobile";
+config.casServerUrl = "http://localhost:8080";
 
 config.storageFn = "local";
-
-// Sample configuration for local HTML5-only testing
-config.uMobileServerUrl = "http://localhost/~athena/dev/umobile-app-phonegap/www";
-config.uMobileServerContext = "/test-data";
-config.loginFn = "mockLogin";
 
 // locally-hosted icons for uMobile modules, keyed by module fname
 config.nativeIcons = {
--- www/lib/umobile/umobile-plugins-web.js
+++ www/lib/umobile/umobile-plugins-web.js
@@ -1,3 +1,8 @@
+// override the settings in config.js to provide mock data and mock login functionality
+config.uMobileServerContext = "/test-data";
+config.loginFn = "mockLogin";
+
+// Mock in-memory implementation of the SessionTracking plugin
 var SessionTracking = {
     time: 0,
     get: function (success, failure) {
