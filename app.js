861ca1bbf89a4fb623bf80ebd89f6caea1f60d5e
Matthew Polizzotti <mpolizzotti@unicon.net>
2013-02-20 22:02:13 -0700

    Added node development server. Added empty grunt file. Restructured JavaScript directories.

---
+++ app.js
@@ -0,0 +1,32 @@
+/*jshint es5:true */
+/*global require:true, __dirname:true, console:true, process:true, setTimeout:true */
+
+// Modules & Variables.
+var express = require('express'),
+	http = require('http'),
+	path = require('path'),
+	app;
+
+// Express configuration.
+app = express();
+
+// All server configurations.
+app.configure(function () {
+	'use strict';
+	// settings.
+	app.set('port', 5000);
+
+	// middleware.
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.errorHandler());
+	app.use(express.static(path.join(__dirname, 'www')));
+});
+
+// Startup.
+http.createServer(app).listen(app.get('port'), function () {
+	'use strict';
+	console.log('Express server listening on port ' + app.get('port'));
+});
--- www/config.js
+++ www/config.js
@@ -1,9 +1,9 @@
 var config = {};
-        
+
 /* BASE SERVER URL */
-config.uMobileServerUrl = "http://10.0.2.2:8080";   // android only, otherwise http://localhost:8080
+//config.uMobileServerUrl = "http://10.0.2.2:8080";   // android only, otherwise http://localhost:8080
 //config.uMobileServerUrl = "https://umobile.unicon.net";   // android only, otherwise http://localhost:8080
-//config.uMobileServerUrl = "http://localhost/~me/umobile/www";  // sample HTML5-only testing URL
+config.uMobileServerUrl = "http://localhost:5000";  // sample HTML5-only testing URL
 
 /* UMOBILE SERVER CONTEXT PATH */
 config.uMobileServerContext = "";
@@ -17,27 +17,27 @@ config.storageFn = "local";
 
 // locally-hosted icons for uMobile modules, keyed by module fname
 config.nativeIcons = {
-    athletics : 'athletics.png',
-    announcements : 'bullhorn.png',
-    calendar : 'calendar.png',
-    "computer-labs" : 'computer_lab.png',
-    courses : 'courses.png',
-    dining : 'dining.png',
-    directory : 'directory.png',
-    laundry : 'laundry.png',
-    library : 'library.png',
-    map : 'map.png',
-    news : 'feed.png',
-    presentations : 'opencast.png',
-    search : 'search.png',
-    stats : 'stats.png',
-    transit : 'transit.png',
-    twitter : 'twitter.png',
-    videos : 'youtube.png',
-    weather : 'weather.png',
-    info : 'default-icon.png'
+	athletics : 'athletics.png',
+	announcements : 'bullhorn.png',
+	calendar : 'calendar.png',
+	"computer-labs" : 'computer_lab.png',
+	courses : 'courses.png',
+	dining : 'dining.png',
+	directory : 'directory.png',
+	laundry : 'laundry.png',
+	library : 'library.png',
+	map : 'map.png',
+	news : 'feed.png',
+	presentations : 'opencast.png',
+	search : 'search.png',
+	stats : 'stats.png',
+	transit : 'transit.png',
+	twitter : 'twitter.png',
+	videos : 'youtube.png',
+	weather : 'weather.png',
+	info : 'default-icon.png'
 };
 
 config.nativeModules = {
-    map : 'map.html'
+	map : 'map.html'
 };
