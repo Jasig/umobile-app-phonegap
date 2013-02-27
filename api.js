411c0b776c11df7b783aafa602c0648c0bbc64e7
Misagh Moayyed <mmoayyed@unicon.net>
2013-02-27 12:44:43 -0700

    pushing docs on github site

---
+++ api.js
@@ -0,0 +1,55 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "App",
+        "Credential",
+        "Module",
+        "ModuleCollection",
+        "Session",
+        "SessionTrackerMock",
+        "State",
+        "bootstrap",
+        "getSession"
+    ],
+    "modules": [
+        "app",
+        "auth",
+        "collection",
+        "model",
+        "storage",
+        "umobile",
+        "view"
+    ],
+    "allModules": [
+        {
+            "displayName": "app",
+            "name": "app"
+        },
+        {
+            "displayName": "auth",
+            "name": "auth"
+        },
+        {
+            "displayName": "collection",
+            "name": "collection"
+        },
+        {
+            "displayName": "model",
+            "name": "model"
+        },
+        {
+            "displayName": "storage",
+            "name": "storage"
+        },
+        {
+            "displayName": "umobile",
+            "name": "umobile",
+            "description": "Namespace for the umobile application."
+        },
+        {
+            "displayName": "view",
+            "name": "view"
+        }
+    ]
+} };
+});
